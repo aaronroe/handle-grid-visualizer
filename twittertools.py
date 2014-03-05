448738f6b239440670b035534994131ac2c5cd18
aaronroe <aaronalkan@gmail.com>
2014-03-05 14:41:36 -0600

    Adds some more organization to twittertools.py

--- twittertools.py
+++ twittertools.py
@@ -2,13 +2,20 @@ import tweepy
 
 class TwitterTimeline:
 	"""Class that represents the Twitter timeline of a user."""
+	def __init__(self, api, user_id):
+		self.timeline = api.user_timeline(user_id)
+		self.last_id = self.timeline.max_id
+
+class TwitterAccessor:
+	"""Class that provides clean access to Twitter data"""
 	def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
 		# Auth using the credentials.
 		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 		auth.set_access_token(access_token, access_token_secret)
 
-		# Set instance of API.
+		# Set class instance for API access.
 		self.api = tweepy.API(auth)
 
 	def testAPI(self):
-		print len(self.api.user_timeline('LOLDyrus'))
+		timeline = TwitterTimeline(self.api, 'LoLDyrus')
+		print timeline.last_id
