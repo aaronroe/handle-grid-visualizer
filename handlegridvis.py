db6013ac8b08e8d1b662f749f5a8d7f4b06abfda
aaronroe <aaronalkan@gmail.com>
2014-03-11 19:51:35 -0500

    Adds a route for tweets

--- handlegridvis.py
+++ handlegridvis.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
+
+@app.route("/tweets/<handle_records>")
+def get_more_tweets(handle_records):
+	return 'Hello World!'
 
 if __name__ == "__main__":
-    app.run()
+  app.run(debug=True)
