7cdece491c7f340a79d567d297f318ba37a3bfae
aaronroe <aaronalkan@gmail.com>
2014-03-11 21:39:00 -0500

    Adds config file and imports it in the controller

--- handlegridvis.py
+++ handlegridvis.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from twittertools import TwitterInterface
+from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,6 +10,7 @@ def index():
 
 @app.route("/tweets/<handle_records>")
 def get_more_tweets(handle_records):
+	interface = TwitterInterface(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 	return 'Hello World!'
 
 if __name__ == "__main__":
